f23d59bdac87ab0628711f702fa9cd4fe2068055
Paul Houghton <paul4hough@gmail.com>
2013-10-07 02:13:09 -0600

    WIP: more experimentation

--- quicktest.bash
+++ quicktest.bash
@@ -1,5 +1,7 @@
 #/bin/bash
 unset GEM_HOME
 unset GEM_PATH
+PATH=/opt/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin
 sudo python bin/install-puppet.py
+head /usr/bin/puppet
 puppet --version
