4b58cf990b3cb3eb29697a6f5d0acd84b88e04e7
Paul Houghton <paul4hough@gmail.com>
2013-10-14 14:40:35 -0600

    Install puppet gem on travis-ci

--- bin/install-puppet.py
+++ bin/install-puppet.py
@@ -149,8 +149,9 @@ def install_puppet(sysname,osname=None,osver=None,osvername=None):
     # Travis needs gems installed with bundle
     if os.environ.get('TRAVIS'):
         gemfile = tempfile.NamedTemporaryFile(mode='w',delete=False)
-        gemfile.write( "source 'https://rubygems.org'\ngem 'puppet'\n")
+        gemfile.write("source 'https://rubygems.org'\ngem 'puppet'\n")
         gemfile.close()
+        sysdo(['cat',gemfile.name])
         sysdo(['bundle','install','--gemfile='+gemfile.name])
         os.remove(gemfile.name)
         sysdo(['gem','install','puppet'])
